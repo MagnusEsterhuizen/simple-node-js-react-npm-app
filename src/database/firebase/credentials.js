bc3757ad940653fac4adf291506d2a5a535f99fc
Magnus Esterhuizen <magnus@mechron.co.za>
2019-10-02 23:50:52 +0200

    test3

---
+++ src/database/firebase/credentials.js
@@ -0,0 +1,14 @@
+//source
+export const sourceCredentials = {
+    apiKey: "AIzaSyAR6d6SnIoJX769-9GFO9jucRIbM2FgP5E",
+    authDomain: "charlie-brown-d8e87.firebaseapp.com",
+    databaseURL: "https://charlie-brown-d8e87.firebaseio.com",
+    projectId: "charlie-brown-d8e87",
+    storageBucket: "charlie-brown-d8e87.appspot.com",
+    messagingSenderId: "739471038787",
+    appId: "1:739471038787:web:9a736b8a1fa5e90aba37f6",
+    measurementId: "G-NV7XWVX3KH"
+}
+
+//destination: duplicate source to here
+export const destinationCredentials = {}
